a4bfee10689f5fbd05fdd699aa1907617e990816
FFS28 <116809250+FFS28@users.noreply.github.com>
2022-11-30 08:27:52 -0800

    2 files modified

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="m-8">
-    <h1 class="text-3xl font-bold underline">
-      Customer Reviews Component
-    </h1>
+    <h1 class="text-3xl font-bold underline">Customer Reviews Component</h1>
     <div class="mt-8">
-      <CeCustomerReviews></CeCustomerReviews>
+      <CeCustomerReviews rate="4" value="10"></CeCustomerReviews>
     </div>
     <div class="mt-24">
       Expected result:
-      <img src="/result.png" alt="Expected result">
+      <img src="/result.png" alt="Expected result" />
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
