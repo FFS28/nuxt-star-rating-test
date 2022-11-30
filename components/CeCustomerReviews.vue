3b9c46fdd49b09028d6eaf0ba81ae28923a8e3ac
FFS28 <116809250+FFS28@users.noreply.github.com>
2022-11-30 10:40:49 -0800

    1 file modified

--- components/CeCustomerReviews.vue
+++ components/CeCustomerReviews.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex">
     <img src="./../static/cert-gold.png" class="h-24 w-24" />
-    <div class="block bg-gray-800 ml-4 py-2 px-6 tip">
+    <div class="block bg-gray-800 ml-6 py-2 px-6 tip">
       <p>Das sagen unsere Kunden</p>
       <div class="flex">
         <label v-for="i in 5">
@@ -25,7 +25,7 @@
             viewBox="0 0 576 512"
           >
             <path
-            class="fill-blue"
+              class="fill-blue"
               d="M381.2 150.3L524.9 171.5C536.8 173.2 546.8 181.6 550.6 193.1C554.4 204.7 551.3 217.3 542.7 225.9L438.5 328.1L463.1 474.7C465.1 486.7 460.2 498.9 450.2 506C440.3 513.1 427.2 514 416.5 508.3L288.1 439.8L159.8 508.3C149 514 135.9 513.1 126 506C116.1 498.9 111.1 486.7 113.2 474.7L137.8 328.1L33.58 225.9C24.97 217.3 21.91 204.7 25.69 193.1C29.46 181.6 39.43 173.2 51.42 171.5L195 150.3L259.4 17.97C264.7 6.954 275.9-.0391 288.1-.0391C300.4-.0391 311.6 6.954 316.9 17.97L381.2 150.3z"
             />
           </svg>
@@ -59,13 +59,18 @@ export default {
 };
 </script>
 <style>
+.tip {
+  background-color: #f0fff0;
+}
 .tip::after {
   position: absolute;
+  margin-left: -43px;
+  margin-top: -60px;
   content: '';
-  height: 10px;
-  width: 10px;
-  border-top: none;
-  border-bottom: none;
-  border-left: none;
+
+  border-top: 20px solid transparent;
+  border-bottom: 20px solid transparent;
+
+  border-right: 20px solid #f0fff0;
 }
 </style>
